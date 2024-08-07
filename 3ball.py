bee181c24222d109373c6ac02b1f73f3f2e2abf3
Aniket Yadav <aniket343yadav@gmail.com>
2024-08-07 14:53:53 +0530

    update

--- 3ball.py
+++ 3ball.py
@@ -10,16 +10,15 @@ new_list = shuffle_list()
 
 def take_guess():
     guess=""
-    while True:
+    while guess not in [1,2,3]:
         guess = int(input("Enter your guess between 1 to 3:"))
-        
     return guess
 
 guess=take_guess()
 
 def check_guess(new_list,guess):
     for i in new_list:
-        if new_list[guess] == "O":
+        if new_list[guess-1] == "O":
             print("Congratulations")
         else:
             print("Sorry")
